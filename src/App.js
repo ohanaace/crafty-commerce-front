ff3f49eade0317dccb814ccbc7a73d535d9150df
Andrey Mudri <andreybeckert@gmail.com>
2023-04-26 20:11:15 -0300

    SignUp Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LogInPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+
+const apiUrl = process.env.REACT_APP_API_URL;
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+export { apiUrl };
