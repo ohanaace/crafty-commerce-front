4f09ff1b5a1b65eba25bd5dd077bdb32e9d5538e
suelen <sreginaclsilva@gmail.com>
2023-04-28 14:31:25 -0300

    feat: search products by type

---
+++ src/pages/Components/CartIcon.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function CartIcon() {
+    return (
+        <DivRedonda >
+            <Cart />
+        </DivRedonda>
+    )
+}
+
+const DivRedonda = styled.div`
+width: 50px;
+height: 50px;
+border-radius: 50%;
+background-color:#F5F5DC;
+display: flex;
+align-items:center;
+position: fixed;
+bottom: 70px;
+right: 25px;
+`
+const Cart = styled(FaShoppingCart)`
+width: 50%;
+height:50%;
+margin-left:10px;
+`
